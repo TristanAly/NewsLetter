d5ff43dc0b74e13cf73125304b879aa77d9eab5a
TristanAly <tristan.aly@gmail.com>
2022-08-30 09:27:48 +0200

    Initial Commit

---
+++ NewsLetter/NewsLetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsLetterApp.swift
+//  NewsLetter
+//
+//  Created by Tristan Aly on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsLetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
